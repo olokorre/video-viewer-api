00739cab65dd81a79c60fb82abb3bba229b069c2
Andrei Ruan Steffens <andreiruansteffens@gmail.com>
2025-04-17 13:29:11 -0300

    refatoração simples

--- source/infra/helper/generateThumbnailFromBase64.ts
+++ source/infra/helper/generateThumbnailFromBase64.ts
@@ -4,23 +4,17 @@ import ffmpeg from 'fluent-ffmpeg';
 import { v4 as uuidv4 } from 'uuid';
 import { promisify } from 'util';
 
-// const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
 const unlink = promisify(fs.unlink);
 
 export async function generateThumbnailFromBase64(tempVideoPath: string): Promise<string> {
     const tempId = uuidv4();
     const tempImagePath = path.resolve(__dirname, `${tempId}.png`);
-
     try {
-        // 1. Salva o vídeo temporariamente
-        // await writeFile(tempVideoPath, videoBuffer);
-
-        // 2. Gera a thumbnail com ffmpeg
         await new Promise<void>((resolve, reject) => {
             ffmpeg(tempVideoPath)
                 .screenshots({
-                    timestamps: ['00:00:01'],
+                    timestamps: ['00:00:25'],
                     filename: path.basename(tempImagePath),
                     folder: path.dirname(tempImagePath),
                     size: '320x240',
@@ -28,14 +22,10 @@ export async function generateThumbnailFromBase64(tempVideoPath: string): Promis
                 .on('end', () => resolve())
                 .on('error', reject);
         });
-
-        // 3. Lê a imagem e converte para base64
         const thumbBuffer = await readFile(tempImagePath);
         const base64Thumbnail = `data:image/png;base64,${thumbBuffer.toString('base64')}`;
-
         return base64Thumbnail;
     } finally {
-        // 4. Limpeza
         await unlink(tempVideoPath).catch(() => { });
         await unlink(tempImagePath).catch(() => { });
     }
